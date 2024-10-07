aaee3b600b4145ea26656fc80411bd43504a4d06
Лена Степанова <lena-sar64@yandex-team.ru>
2024-10-07 11:33:51 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,11 @@
+URL_SERVICE = "https://5a63fa94-f771-44f9-8b0b-79038633be91.serverhub.praktikum-services.ru"
+
+DOC_PATH = "/docs/"
+
+LOG_MAIN_PATH = "/api/logs/main/"
+
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+
+CREATE_USER_PATH = "/api/v1/users/"
+
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
